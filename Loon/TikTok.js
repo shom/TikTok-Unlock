f23b0491f1301cbdbd539909d1598498ff718222
shom <suran.x@gmail.com>
2025-10-23 02:00:09 +0800

    更新 TikTok.js

--- Loon/TikTok.js
+++ Loon/TikTok.js
@@ -13,7 +13,7 @@ if (/(tnc|dm).+\.[^\/]+\.com\/\w+\/v\d\/\?/.test(url)) {
   };
   $done({response});
 } else if (/_region=CN&|&mcc_mnc=4/.test(url)) {
-  url = url.replace(/_region=CN&/g,`_region=${loc}&`).replace(/&mcc_mnc=4/g,"&mcc_mnc=2");
+  url = url.replace(/_region=CN/g,`_region=${loc}&`).replace(/&mcc_mnc=4/g,"&mcc_mnc=2");
   const response = {
     status: 307,
     headers: {Location: url},
